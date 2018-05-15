edf09b6970d34b73a2b1ec443048facd3569ff00
Alessandro Morato <alessandro.morato@finantix.com>
2018-05-15 08:37:35 +0200

    CORE-3229 Oracle 11g doesn't support TIMESTAMP WITHOUT TIME ZONE data type
    Oracle 11g doesn't support WITHOUT clause in TIMESTAMP data type (https://docs.oracle.com/cd/B19306_01/server.102/b14225/ch4datetime.htm#sthref389)
    For this reason replace TIMESTAMP WITHOUT TIME ZONE with TIMESTAMP in case of Oracle database.

--- liquibase-core/src/main/java/liquibase/datatype/core/TimestampType.java
+++ liquibase-core/src/main/java/liquibase/datatype/core/TimestampType.java
@@ -123,9 +123,15 @@ public class TimestampType extends DateTimeType {
             OracleDatabase))) || (database instanceof HsqlDatabase) || (database instanceof H2Database)){
             String additionalInformation = this.getAdditionalInformation();
 
-            if ((additionalInformation != null) && (database instanceof PostgresDatabase)) {
-                if (additionalInformation.toUpperCase(Locale.US).contains("TIMEZONE")) {
-                    additionalInformation = additionalInformation.toUpperCase(Locale.US).replace("TIMEZONE", "TIME ZONE");
+            if (additionalInformation != null) {
+                String additionInformation = additionalInformation.toUpperCase(Locale.US);
+                if ((database instanceof PostgresDatabase) && additionInformation.contains("TIMEZONE")) {
+                    additionalInformation = additionInformation.replace("TIMEZONE", "TIME ZONE");
+                }
+                // CORE-3229 Oracle 11g doesn't support WITHOUT clause in TIMESTAMP data type
+                if ((database instanceof OracleDatabase) && additionInformation.startsWith("WITHOUT")) {
+                    // https://docs.oracle.com/cd/B19306_01/server.102/b14225/ch4datetime.htm#sthref389
+                    additionalInformation = null;
                 }
             }
 
